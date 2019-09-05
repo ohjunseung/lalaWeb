44c295b19ab2a0920caa9369ee7afdac5317e0c0
CyberThink777 <alexanderm61@gmail.com>
2019-09-05 10:41:55 +0800

    coba

--- src/lalaWeb/UserServlet.java
+++ src/lalaWeb/UserServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Set;
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
@@ -21,7 +22,7 @@ public class UserServlet extends HttpServlet {
             if (DBUtil.checkAdmin(user)) {
                 String action = req.getParameter("action");
                 if (action == null) {
-                	req.getRequestDispatcher("/WEB-INF/editpass.jsp").forward(req, resp);
+                    req.getRequestDispatcher("/WEB-INF/editpass.jsp").forward(req, resp);
                 }
                 if (action.equals("add")) {
                     req.getRequestDispatcher("/WEB-INF/addadmin.jsp").forward(req, resp);
@@ -35,24 +36,26 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
+        String url = "";
         try {
             User user = (User) session.getAttribute("user");
             if (DBUtil.checkAdmin(user)) {
                 String action = req.getParameter("action");
                 if (action == null) {
-                    User newUser = new User(req.getParameter("email"),req.getParameter("pass"));
-                    DBUtil.editUser(user,newUser);
-                    resp.sendRedirect(getServletContext().getContextPath() + "/login");
+                    User newUser = new User(req.getParameter("email"), req.getParameter("pass"));
+                    DBUtil.editUser(user, newUser);
+                    url = "/login";
                 }
                 if (action.equals("add")) {
-                    User newUser = new User(req.getParameter("email"),req.getParameter("pass"));
-                    if (DBUtil.register(newUser)) {
-                        resp.sendRedirect(getServletContext().getContextPath() + "/admin");
-                    } else resp.sendRedirect(getServletContext().getContextPath() + "/user?action=add&incorrect=true");
+                    User newUser = new User(req.getParameter("email"), req.getParameter("pass"));
+                    if (DBUtil.register(newUser))
+                        url = "/admin";
+                    else url = "/user?action=add&incorrect=true";
                 }
-            } else resp.sendRedirect(getServletContext().getContextPath() + "/login");
+            } else url = "/login";
         } catch (NullPointerException e) {
-            resp.sendRedirect(getServletContext().getContextPath() + "/login");
+            url = "/login";
         }
+        resp.sendRedirect(getServletContext().getContextPath() + url);
     }
 }
